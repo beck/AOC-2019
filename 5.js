21652cb6cd398145b73f9a296a639c5aba22d1c2
Doug Beck <doug@douglasbeck.com>
2019-12-07 22:01:37 -0500

    Refactor computer

--- 5.js
+++ 5.js
@@ -1,34 +1,84 @@
 #!/usr/bin/env node
 require('clear')();
 const rl = require('readlines');
-let input = rl.readlinesSync('./run-in.txt').slice(0, -1);
-input = input[0].split(',').map(Number);
+let input = rl.readlinesSync('./5-in.txt').slice(0, -1);
+input = input[0].split(',');
 
-const run = mem => {
-  let i = 0;
-  while (i < mem.length) {
-    const [cmd, l, r, t] = mem.slice(i, i + 4);
-    i += 4;
+class IntcodeComputer {
+
+  constructor(mem) {
+    this.mem = mem;
+    this.ip = 0;
+    this.halted = false;
+    this.io = null;
+  }
+
+  readint(addr = null) {
+    if (addr !== null) return Number(this.mem[addr]);
+    return Number(this.mem[this.ip++]);
+  }
+
+  readcmd() {
+    const cmd = this.mem[this.ip++];
+    return [Number(cmd), 'pos'];
+  }
+
+  next() {
+    const [cmd, ...modes] = this.readcmd();
     if (cmd === 1) {
-      mem[t] = mem[l] + mem[r];
-      continue;
+      this.add(modes);
+      return;
     }
     if (cmd === 2) {
-      mem[t] = mem[l] * mem[r];
-      continue;
+      this.multiply(modes);
+      return;
     }
     if (cmd === 3) {
-      // TBD
-      continue;
+      this.input(modes);
+      return;
     }
     if (cmd === 4) {
-      // TBD
-      continue;
+      this.output(modes);
+      return;
     }
-
-    return mem[0];
+    if (cmd !== 99) {
+      console.error('bad halt');
+    }
+    this.halted = true;
   }
+
+  input() {
+    const param = this.readint();
+    this.mem[param] = this.io;
+  }
+
+  output() {
+    const param = this.readint();
+    this.io = this.readint(param);
+  }
+
+  add() {
+    const a = this.readint();
+    const b = this.readint();
+    const c = this.readint();
+    this.mem[c] = this.readint(a) + this.readint(b);
+  }
+
+  multiply() {
+    const a = this.readint();
+    const b = this.readint();
+    const c = this.readint();
+    this.mem[c] = this.readint(a) * this.readint(b);
+  }
+
+  run() {
+    while (!this.halted) this.next();
+    //console.log('result', this.mem[0]);
+    return this.mem[0];
+  }
+
 }
+
 
 const main = () => {
   for (let i = 0; i <= 99; i++) {
@@ -36,7 +86,8 @@ const main = () => {
       const mem = [...input];
       mem[1] = i;
       mem[2] = j;
-      if (run(mem) === 19690720) {
+      const computer = new IntcodeComputer(mem);
+      if (computer.run() === 19690720) {
         console.log(i, j);
         return;
       }
---
+++ 7.2.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+require('clear')();
+const rl = require('readlines');
+let input = rl.readlinesSync('./6-in.txt').slice(0, -1);
+
